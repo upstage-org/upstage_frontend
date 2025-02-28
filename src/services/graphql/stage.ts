91fd53fad4e5e418f53844a1aed0ef7efaee0c4f
webdev604 <webdev.604@hotmail.com>
2025-02-28 16:03:19 +0800

    events query

--- src/services/graphql/stage.ts
+++ src/services/graphql/stage.ts
@@ -212,7 +212,7 @@ export default {
               scenes {
                 ...sceneFragment
               }
-              events(performanceId: $performanceId) {
+              events {
                   id
                   topic
                   payload
