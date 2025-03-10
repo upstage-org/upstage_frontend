db0a024e71b2ce61e8d1e83b1e370f4625057a27
webdev604 <webdev.604@hotmail.com>
2025-03-10 22:12:59 +0800

    media permissions system #1357

--- src/components/media/QuickStageAssignment.vue
+++ src/components/media/QuickStageAssignment.vue
@@ -13,10 +13,17 @@ const props = defineProps({
     required: true,
   },
 });
-
+const stages = ref([]);
 const visible = ref(false);
-const keyword = ref("");
 const isAdmin = computed(() => store.getters["user/isAdmin"]);
+const note = computed(() => {
+  try {
+    const description = JSON.parse(props.media.description);
+    return description?.note;
+  } catch (err) {
+    return ""
+  }
+});
 const { result, loading } = useQuery(
   gql`
     {
@@ -45,26 +52,22 @@ const { result, loading } = useQuery(
 
 const dataSource = computed(() => {
   if (result.value) {
-    const s = keyword.value.trim();
     return result.value.stages.edges
       .filter((el: any) => {
-        return isAdmin.value ? true : (el.permission == "editor" || el.permission == "owner")
-      }).filter((node: any) => {
-        if (node.name.toLowerCase().includes(s)) {
-          return true;
-        }
-        return false;
+        return (isAdmin.value ? true : (el.permission == "editor" || el.permission == "owner")) && !props.media.stages.some((s) => s.id === el.id)
       });
   }
   return [];
 });
-
+const handleFilterStageName = (keyword: string, option: any) => {
+  return option.label.toLowerCase().includes(keyword.toLowerCase());
+};
 const { mutate } = useMutation<
   { quickAssignMutation: { asset: Media } },
-  { assetId: string; stageId: string }
+  { assetId: string; stageIds: [string] }
 >(gql`
-  mutation QuickAssignMutation($assetId: ID!, $stageId: ID!) {
-    quickAssignMutation(assetId: $assetId, stageId: $stageId) {
+  mutation QuickAssignMutation($assetId: ID!, $stageIds: [ID]!) {
+    quickAssignMutation(assetId: $assetId, stageIds: $stageIds) {
       success
       message
     }
@@ -72,16 +75,20 @@ const { mutate } = useMutation<
 `);
 
 const refresh = inject("refresh", () => { });
-const quickAssign = async (stage: Stage) => {
+
+const handleOk = async () => {
+  const stageIds: any = stages.value;
   await mutate({
     assetId: props.media.id,
-    stageId: stage.id,
+    stageIds: stageIds,
   });
   message.success(
-    `${props.media.name} had been assigned to ${stage.name} successfully!`,
+    `${props.media.name} had been assigned successfully!`,
   );
   refresh();
-};
+  stages.value = [];
+  visible.value = false;
+}
 </script>
 
 <template>
@@ -89,46 +96,26 @@ const quickAssign = async (stage: Stage) => {
     <plus-circle-outlined />Assign to stage
   </a-button>
   <a-modal v-model:visible="visible" class="custom-class" style="color: red"
-    title="Assign this media to one of your stages" :width="600">
+    title="Assign this media to one of your stages" :width="600" @ok="handleOk"
+    :okButtonProps="{ disabled: stages.length <= 0 }">
     <div class="flex" style="align-items: center; justify-content: flex-start;">
       <div style="max-width: 350px;">
-      <MediaPreview v-if="media.assetType" :media="media as Media" />
+        <MediaPreview v-if="media.assetType" :media="media as Media" />
       </div>
       &nbsp;&nbsp;&nbsp;
-      <h3 style="color: black;">{{ media.name }}</h3>
+      <h4 style="color: black;">{{ media.name }}</h4>
     </div>
-    <br />
+    <h3 style="color: black; margin-top: 20px;">{{ media.copyrightLevel == 1 && note }}</h3>
     <div class="flex">
-      <a-input-search class="mr-2" placeholder="Stage name" v-model:value="keyword"></a-input-search>
+      <a-select allowClear showArrow :filterOption="handleFilterStageName" mode="tags" style="width: 100%;"
+        placeholder="Stage name" :loading="loading" v-model:value="stages" :options="dataSource
+          ? dataSource.map((e: any) => ({
+            value: e.id,
+            label: e.name,
+          }))
+          : []
+          ">
+      </a-select>
     </div>
-    <a-list class="demo-loadmore-list" :loading="loading" item-layout="horizontal" :data-source="dataSource">
-      <template #renderItem="{ item }">
-        <a-list-item class="p-0">
-          <template #actions>
-            <a-tooltip v-if="media.stages.some((s) => s.id === item.id)" title="Already assigned" placement="topRight">
-              <a-button disabled>
-                <check-outlined />
-              </a-button>
-            </a-tooltip>
-            <a-tooltip v-else :title="`Assign ${media.name} to this stage`" placement="topRight">
-              <smart-button :action="() => quickAssign(item)" type="primary">
-                <plus-outlined />
-              </smart-button>
-            </a-tooltip>
-          </template>
-          <a-list-item-meta>
-            <template #title>
-              <a-button type="text" class="p-0" style="position: relative; top: 8px">{{ item.name }}</a-button>
-            </template>
-            <template #description>
-              <small style="position: relative; top: -4px">
-                Created by {{ item.owner.displayName || item.owner.username }}
-                <d-date :value="item.createdOn"></d-date>
-              </small>
-            </template>
-          </a-list-item-meta>
-        </a-list-item>
-      </template>
-    </a-list>
   </a-modal>
 </template>
