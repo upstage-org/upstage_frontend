421821f25bf014cef409ddf4bd90c90c0445fd9f
nnguyenquangg <nguyenquang06012000@gmail.com>
2025-04-09 17:11:15 +0700

    feat: apply service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,13 +9,3 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(fetch(event.request));
 });
-
-self.addEventListener('message', (event) => {
-  if (event.data?.type === 'CHECK_UPDATE') {
-    self.clients.matchAll().then(clients => {
-      clients.forEach(client => {
-        client.postMessage({ type: 'VERSION_UPDATE', version: '1.0.2' });
-      });
-    });
-  }
-});
