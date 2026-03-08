527238b16914c816c6a8930f7f0b28221930be7c
Gloria W <gloriajw66@gmail.com>
2026-03-09 11:41:18 +1300

    docker scope

--- run_front_end_dev.sh
+++ run_front_end_dev.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-build_dir=/frontend_app/build
+set -a
 
-rm -rf /frontend_app/dist
-mkdir /frontend_app/dist
+build_dir=/frontend_app_dev/build
+
+rm -rf /frontend_app_dev/dist
+mkdir /frontend_app_dev/dist
 
 # index.html is kept under initial_scripts/build_files, and is copied up 
 # to the topmost directory during the front end build.
@@ -12,9 +14,9 @@ cp -r ./initial_scripts/build_files/* $build_dir
 cp -r ./src $build_dir
 cp -r ./public $build_dir
 cp -r ./public/favicon.ico $build_dir
-cp -r ./docker-compose.yaml $build_dir
+cp -r ./docker-compose-dev.yaml $build_dir
 
-cp  /frontend_app/.env /frontend_app/dist/.env # Cannot mount .env from root dir. Docker copies it out.
+cp  /frontend_app_dev/.env /frontend_app_dev/dist/.env # Cannot mount .env from root dir. Docker copies it out.
 
 cd $build_dir
 
@@ -22,7 +24,8 @@ echo "This build may take up to three minutes. It may be necessary to run 'docke
 
 echo "Building..."
 
-docker compose down
-docker compose up -d
+docker compose -f ./docker-compose-dev.yaml -p upstage-frontend-dev down
+docker compose -f ./docker-compose-dev.yaml -p upstage-frontend-dev up -d
+docker compose -f ./docker-compose-dev.yaml -p upstage-frontend-dev ps
 
 echo "Done"
